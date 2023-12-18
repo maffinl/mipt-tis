6df9bd955a223dab5bd10caf588cf9564a4de31e
maffinl <103385315+maffinl@users.noreply.github.com>
2023-12-18 09:50:11 +0300

    Update python_main.py

--- python_main.py
+++ python_main.py
@@ -10,13 +10,14 @@ def main():
     ret = render_template("History_country_people.html")
     return ret
 
-@app.route("/")
+@app.route('/country')
 def country(country_id=None):
     country_info = bd.get_country(country_id=country_id)
     language_info = sorted(bd.get_language(country_id), key=lambda x: x[1], reverse=True)
     events_info = sorted(bd.get_events(country_id=country_id), key=lambda x: x[2])
-    persons_info = sorted(bd.get_person(country_id=country_id), key=lambda x: x[2])
-    ret = render_template('country.html', country_info=country_info, language_info=language_info,>    return ret
+    persons_info = sorted(bd.get_persons(country_id=country_id), key=lambda x: x[2])
+    ret = render_template('country.html', country_info=country_info, language_info=language_info, events_info=events_info, persons_info=persons_info)
+    return ret
 
 
 app.run(host='0.0.0.0', port=6011)
