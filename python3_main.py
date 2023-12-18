be309f439480eccb49e8c5f8500d04194d21bc8f
maffinl <103385315+maffinl@users.noreply.github.com>
2023-12-18 09:52:21 +0300

    Update python3_main.py

--- python3_main.py
+++ python3_main.py
@@ -1,15 +1,15 @@
-
 from flask import Flask, render_template, request
 from python3_data import Historic_Data
 
 app = Flask(__name__)
-python3_data = Historic_Data()
+bd = Historic_Data()
 
 
 @app.route("/")
 def main():
     ret = render_template("History_country_people.html")
     return ret
+
 @app.route("/countries", methods=['GET', 'POST'])
 def countries():
     if request.method == 'GET':
@@ -27,7 +27,8 @@ def country(country_id=None):
     language_info = sorted(bd.get_language(country_id), key=lambda x: x[1], reverse=True)
     events_info = sorted(bd.get_events(country_id=country_id), key=lambda x: x[2])
     persons_info = sorted(bd.get_persons(country_id=country_id), key=lambda x: x[2])
-    ret = render_template("country.html",country_info=country_info[0], language_info=language_inf>    return ret
+    ret = render_template("country.html",country_info=country_info[0], language_info=language_info, events_info=events_info, persons_info=persons_info)
+    return ret
 
 
 @app.route("/persons", methods=['GET', 'POST'])
@@ -40,12 +41,15 @@ def persons():
         persons_list = sorted(bd.get_persons(name_filter=name_filter), key=lambda x: x[1])
         ret = render_template("persons.html", persons_list=persons_list)
     return ret
+
+
 @app.route("/person/<person_id>")
 def person(person_id=None):
     person_info = bd.get_persons(person_id=person_id)
     countries_info = sorted(bd.get_countries(person_id=person_id), key=lambda x: x[2])
     events_info = sorted(bd.get_events(person_id=person_id), key=lambda x: x[2])
-    ret = render_template("persons.html", countries_info=countries_info, events_info=events_info,>    return ret
+    ret = render_template("person.html", countries_info=countries_info, events_info=events_info, person_info=person_info[0])
+    return ret
 
 
 @app.route("/events", methods=['GET', 'POST'])
@@ -65,7 +69,8 @@ def event(event_id=None):
     event_info = bd.get_events(event_id=event_id)
     persons_info = sorted(bd.get_persons(event_id=event_id), key=lambda x: x[2])
     countries_info = sorted(bd.get_countries(event_id=event_id), key=lambda x: x[1])
-    ret = render_template("events.html", countries_info=countries_info, event_info=event_info[0],>    return ret
+    ret = render_template("event.html", countries_info=countries_info, event_info=event_info[0], persons_info=persons_info)
+    return ret
 
 
 app.run(host='0.0.0.0', port=6011)
